412c428cf89c67a43644a9d2943d019300c2d1e1
chaitanyatr <thathireddy14182@mail.npu.edu>
2017-06-01 11:24:07 -0700

    commiting bean classes
    first commit

---
+++ PersonalInfoBean.java
@@ -0,0 +1,29 @@
+package com.java.bn;
+
+public class PersonalInfoBean {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	public PersonalInfoBean() {
+		super();
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
